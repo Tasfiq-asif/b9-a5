d355ef51b6079692210d31bcf36341702bbaee32
Tasfiq Sunny <tasfiq.asif.23.com>
2024-02-18 13:55:36 +0600

    Seat Layout complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+      extend: {
+          gridTemplateColumns: {
+              '4': 'repeat(4, minmax(0, 140px))',
+          }
+      },
+  },
+  plugins: [],
+}
