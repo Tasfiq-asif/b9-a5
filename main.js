514955f6d7339135e4cbe29484d1a2ccbb9da167
Tasfiq Sunny <tasfiq.asif.23.com>
2024-02-18 23:38:47 +0600

    adding form functionality

--- main.js
+++ main.js
@@ -2,6 +2,34 @@
 
 
 
+// Button select
+
+ // Get all the buttons
+ const buttons = document.querySelectorAll('.bus-seat button');
+    
+ // Initialize counter for selected buttons
+ let selectedCount = 0;
+ 
+ // Add click event listener to each button
+ buttons.forEach(button => {
+     button.addEventListener('click', () => {
+         // Toggle background color between green and default color
+         if (!button.classList.contains('bg-green-500') && selectedCount < 4) {
+             button.classList.add('bg-green-500');
+             selectedCount++;
+             console.log(selectedCount)
+         } else {
+            
+            if (button.classList.contains('bg-green-500')) {
+                button.classList.remove('bg-green-500'); 
+                    selectedCount--;
+                    console.log(selectedCount)
+            }
+        }
+     });
+ });
+
+
 // Form button functionality
 
 function checkInputs() {
