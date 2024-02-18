55abbbb106814b834b7db12a8258a0fac6a4b804
Tasfiq Sunny <tasfiq.asif.23.com>
2024-02-19 00:15:34 +0600

    adding dynamic seat change functionality

--- main.js
+++ main.js
@@ -6,9 +6,21 @@
 
  // Get all the buttons
  const buttons = document.querySelectorAll('.bus-seat button');
-    
+ 
+ // seat to add
+
+ const seatAdd = document.getElementById('seat-add')
+
+ // seat to decrease
+
+ 
+
  // Initialize counter for selected buttons
  let selectedCount = 0;
+ // Get the seat left element
+ const seatLeftElement = document.getElementById('seats-left');
+ // Get the current seat left value
+ let seatLeft = parseInt(seatLeftElement.innerText);
  
  // Add click event listener to each button
  buttons.forEach(button => {
@@ -17,15 +29,20 @@
          if (!button.classList.contains('bg-green-500') && selectedCount < 4) {
              button.classList.add('bg-green-500');
              selectedCount++;
-             console.log(selectedCount)
+             seatLeft--;
+             
+             console.log(seatLeft)
          } else {
             
             if (button.classList.contains('bg-green-500')) {
                 button.classList.remove('bg-green-500'); 
                     selectedCount--;
+                    seatLeft++
                     console.log(selectedCount)
             }
         }
+        seatAdd.innerText =selectedCount
+        document.getElementById('seats-left').innerText = seatLeft
      });
  });
 
